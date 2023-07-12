cde297663366c4f977840e156ddc174a792fa3b6
Nabin <thenabinbohora@gmail.com>
2023-07-12 20:16:26 +0930

    project finished

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ menu.addEventListener("click", () => {
     menu.classList.toggle("active");
     navLinks.classList.toggle("active");
 })
+
+document.querySelectorAll(".nav__link").forEach(n => 
+    n.addEventListener("click", () => {
+        menu.classList.remove("active");
+        navLinks.classList.remove("active");
+}))
